9d56eed9fd804f658dc82bd99b9c61f9fa42bd7f
Kevin Kamimura <kevin.t.kamimura@gmail.com>
2017-04-24 21:05:30 +0200

    Removed react router and other fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/layouts/Home';
-import Portfolio from './components/presentation/Portfolio';
 
 const App = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={Portfolio} />
-      <Route path="/skillset" component={Portfolio} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Portfolio} />
-    </div>
-  </Router>
+  <Home />
 );
 
 export default App;
